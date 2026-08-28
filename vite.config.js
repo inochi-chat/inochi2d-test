b14199e0fbb516ec7992bbbdde5be0ff4c873de7
inochi-chat <soraneko916@gmail.com>
2026-08-29 01:21:32 +0900

    Update Vite config to set base path
    Removed library build configuration and set base path.

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,5 @@
-import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    build: {
-        lib: {
-            entry: resolve(__dirname, 'lib/main.ts'),
-            name: "inochi2d",
-            fileName: (format) => `inochi2d.${format}.js`
-        }
-    }
+  base: '/inochi2d-test/',
 });
