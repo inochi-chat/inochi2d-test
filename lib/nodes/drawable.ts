0bdff03b8332bb832a24b77f4d9421cf139b4f6a
inochi-chat <soraneko916@gmail.com>
2026-08-29 10:58:39 +0900

    Update drawable.ts

--- lib/nodes/drawable.ts
+++ lib/nodes/drawable.ts
@@ -116,7 +116,7 @@ export class Part extends Drawable {
             const { _constant } = blendModeData;
 
             // If there's textures
-            if (partTextures) {
+            if (partTextures.length > 0 && partTextures[0]) {
                 // console.log(blendModeData._blendmode.valueOf() + ", " + _constant.valueOf());
                 const texture = partTextures[0]; // Using the first texture as an example
 
