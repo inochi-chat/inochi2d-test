a8bdfc6bda97b6168543c6ca66f558f7db1df3c6
inochi-chat <soraneko916@gmail.com>
2026-08-31 01:04:45 +0900

    Add build configuration with multiple entry points

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,13 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/inochi2d-test/',
+
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        faceTest: 'face-test.html',
+      },
+    },
+  },
 });
