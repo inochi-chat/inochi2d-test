33767e755467812f0ec594a0f89533d9cfca1702
inochi-chat <soraneko916@gmail.com>
2026-09-01 21:12:56 +0900

    Update test.ts

--- lib/test/test.ts
+++ lib/test/test.ts
@@ -1,52 +1,11 @@
-import * as Inochi2D from '../main'
-import * as THREE from 'three';
+const puppet = await Inochi2D.INP.inImportFromURL('testplay2.inp');
 
-const scene = new THREE.Scene();
+console.log("PUPPET LOADED");
+console.log(puppet);
 
-const aspectRatio = window.innerWidth / window.innerHeight;
-
-const cameraWidth = 3000;
-const cameraHeight = cameraWidth / aspectRatio;
-
-const camera = new THREE.OrthographicCamera(
-    cameraWidth / -2,
-    cameraWidth / 2,
-    cameraHeight / 2,
-    cameraHeight / -2,
-    0.01,
-    10000
+Inochi2D.Renderer.renderPuppet(
+    puppet,
+    scene,
+    camera,
+    renderer
 );
-
-camera.position.set(0, 1, 500);
-
-const renderer = new THREE.WebGLRenderer({
-    antialias: true
-});
-
-renderer.setSize(
-    window.innerWidth,
-    window.innerHeight
-);
-
-document.body.appendChild(renderer.domElement);
-
-async function loadPuppet() {
-    try {
-        const puppet = await Inochi2D.INP.inImportFromURL('testplay2.inp');
-
-        console.log("PUPPET LOADED");
-        console.log(puppet);
-
-        Inochi2D.Renderer.renderPuppet(
-            puppet,
-            scene,
-            camera,
-            renderer
-        );
-
-    } catch (error) {
-        console.error("PUPPET LOAD ERROR", error);
-    }
-}
-
-loadPuppet();
