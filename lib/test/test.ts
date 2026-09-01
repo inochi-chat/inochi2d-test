27db3fad24f2c88e60bd049b88871705e488c405
inochi-chat <soraneko916@gmail.com>
2026-09-01 21:02:10 +0900

    Update test.ts

--- lib/test/test.ts
+++ lib/test/test.ts
@@ -1,21 +1,13 @@
-/*
-    Testing script.
-
-    Copyright © 2023, Inochi2D Project
-    Distributed under the 2-Clause BSD License, see LICENSE file.
-    
-    Authors: FartDraws
-*/
-
 import * as Inochi2D from '../main'
 import * as THREE from 'three';
 
 const scene = new THREE.Scene();
+
 const aspectRatio = window.innerWidth / window.innerHeight;
 
-// Set up the parameters for the orthographic camera
 const cameraWidth = 3000;
 const cameraHeight = cameraWidth / aspectRatio;
+
 const camera = new THREE.OrthographicCamera(
     cameraWidth / -2,
     cameraWidth / 2,
@@ -25,24 +17,36 @@ const camera = new THREE.OrthographicCamera(
     10000
 );
 
-// Set camera position
 camera.position.set(0, 1, 500);
 
-const renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.setSize(window.innerWidth, window.innerHeight);
+const renderer = new THREE.WebGLRenderer({
+    antialias: true
+});
+
+renderer.setSize(
+    window.innerWidth,
+    window.innerHeight
+);
+
 document.body.appendChild(renderer.domElement);
 
 async function loadPuppet() {
-    const puppet = await Inochi2D.INP.inImportFromURL('testplay2.inp');
+    try {
+        const puppet = await Inochi2D.INP.inImportFromURL('testplay2.inp');
 
-    console.log("Loaded " + puppet.meta + "!");
+        console.log("PUPPET LOADED");
+        console.log(puppet);
 
-    Inochi2D.Renderer.renderPuppet(
-        puppet,
-        scene,
-        camera,
-        renderer
-    );
+        Inochi2D.Renderer.renderPuppet(
+            puppet,
+            scene,
+            camera,
+            renderer
+        );
+
+    } catch (error) {
+        console.error("PUPPET LOAD ERROR", error);
+    }
 }
 
 loadPuppet();
