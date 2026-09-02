5c999ccabc7d39883bd9d37340e3fcea1febd6cf
inochi-chat <soraneko916@gmail.com>
2026-09-02 10:27:37 +0900

    Update test.ts

--- lib/test/test.ts
+++ lib/test/test.ts
@@ -3,6 +3,20 @@ const puppet = await Inochi2D.INP.inImportFromURL('testplay2.inp');
 console.log("PUPPET LOADED");
 console.log(puppet);
 
+console.log("ROOT NODE:", puppet.rootNode);
+console.log("NODES:", puppet.nodes);
+console.log("NODE COUNT:", puppet.nodes.length);
+
+for (const node of puppet.nodes) {
+    console.log(
+        "NODE:",
+        node.type,
+        node.name,
+        node.uuid,
+        node instanceof THREE.Mesh ? "MESH" : "NO MESH"
+    );
+}
+
 Inochi2D.Renderer.renderPuppet(
     puppet,
     scene,
