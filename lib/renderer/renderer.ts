d45880324b560c8eb05eb7e5dcfec88d6d5c0e75
inochi-chat <soraneko916@gmail.com>
2026-09-02 20:58:06 +0900

    Update renderer.ts

--- lib/renderer/renderer.ts
+++ lib/renderer/renderer.ts
@@ -20,29 +20,29 @@ export const blend_modes = [
     { _blendmode: BlendMode.Multiply, _constant: THREE.MultiplyBlending }
 ];
 
-
-// Function to recursively add nodes to the scene
 function createNode(
     node: Node | Drawable,
     scene: THREE.Object3D,
     parent: THREE.Object3D,
     textures: THREE.Texture[]
 ) {
-    
     node.create();
     node.update();
 
     parent.add(node.threeObj);
 
     for (let child of node.children) {
-        createNode(child, scene, node.threeObj, textures);
+        createNode(
+            child,
+            scene,
+            node.threeObj,
+            textures
+        );
     }
 
     return node.threeObj;
 }
 
-
-// Function to render a Puppet
 export function renderPuppet(
     puppet: Puppet,
     scene: THREE.Scene,
@@ -66,7 +66,10 @@ export function renderPuppet(
         rootNode.children[0]?.children.length
     );
 
-    console.log("ROOT OBJ TYPE:", rootNode.type);
+    console.log(
+        "ROOT OBJ TYPE:",
+        rootNode.type
+    );
 
     console.log(
         "ROOT OBJ CHILDREN:",
@@ -186,18 +189,15 @@ export function renderPuppet(
     );
 
     scene.add(rootNode);
-    
 
     const animate = function () {
-    requestAnimationFrame(animate);
+        requestAnimationFrame(animate);
 
-    renderer.setClearColor(0x00ff00);
-
-    renderer.render(
-        scene,
-        camera
-    );
-};
+        renderer.render(
+            scene,
+            camera
+        );
+    };
 
     animate();
 
