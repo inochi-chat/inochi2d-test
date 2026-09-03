ae950fdb579b41f7b8e6de47cf7e2a87910b6b92
inochi-chat <soraneko916@gmail.com>
2026-09-03 09:07:02 +0900

    Update test-new.ts

--- lib/test/test-new.ts
+++ lib/test/test-new.ts
@@ -17,7 +17,7 @@ const camera = new THREE.OrthographicCamera(
     10000
 );
 
-camera.position.set(0, -1000, 500);
+camera.position.set(0, -3000, 500);
 
 const renderer = new THREE.WebGLRenderer({
     antialias: true
